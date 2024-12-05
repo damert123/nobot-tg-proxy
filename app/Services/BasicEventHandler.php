1dd94ad071794d61b6f895b3e82b28bea9dfeb34
damert123 <damert337@gmail.com>
2024-12-05 10:45:21 +0300

    Поменял домен crm

--- app/Services/BasicEventHandler.php
+++ app/Services/BasicEventHandler.php
@@ -259,7 +259,7 @@ class BasicEventHandler extends SimpleEventHandler
                 }
 
 
-                $response = Http::asForm()->post('https://testservice123.planfix.ru/webchat/api', $data);
+                $response = Http::asForm()->post('https://agencylemon.planfix.ru/webchat/api', $data);
 
                 if ($response->successful()){
                     Log::channel('planfix-messages')->info('Сообщение успешно отправлено в PlanFix', [
