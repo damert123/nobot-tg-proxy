e9caca8f1ded6229784732ee7dd261b076619a6a
damert123 <damert337@gmail.com>
2024-12-09 18:31:20 +0300

    логи истории

--- app/Http/Controllers/PlanfixChatController.php
+++ app/Http/Controllers/PlanfixChatController.php
@@ -262,7 +262,10 @@ class PlanfixChatController extends Controller
                         ]
                     ],
                 ]);
+
                 sleep(1);
+                $resHistory = $madelineProto->messages->getHistory($chatId);
+                Log::channel('planfix-messages')->info("ИСТОРИЯ", [$resHistory]);
 
                 Log::channel('planfix-messages')->info("ТЕКСТ ИЗ PLANFIX to Telegram chat", [$resultMessage['id']]);
 
