d817d6125a45b0445cf56caceac93dd8c3e94617
damert123 <damert337@gmail.com>
2025-01-20 14:06:30 +0300

    проверкаавы123каыв

--- app/Console/Commands/addToStream.php
+++ app/Console/Commands/addToStream.php
@@ -26,40 +26,30 @@ class addToStream extends Command
      */
     public function handle()
     {
-        // Настройки
         $streamName = 'mystream';
 
         // XADD - добавление записи
-        $message = [
-            'name' => 'Test message',
-            'time' => now(),
-        ];
-
-        // Правильное добавление параметров в команду XADD
-        $args = [$streamName, '*']; // имя потока и символ * для авто-генерации id
-
-        foreach ($message as $key => $value) {
-            // Добавляем пару ключ-значение
-            $args[] = $key;
-            $args[] = $value;
-        }
-
         $this->info("Adding message to stream...");
-        $response = Redis::command('XADD', $args);
 
-        // Проверим результат добавления
-        $this->info("Message added with ID: " . $response);
+        $response = Redis::command('XADD', [
+            $streamName,
+            '*',                // Генерация уникального ID
+            'name', 'Test message', // Поля и их значения
+            'time', now()->toDateTimeString(),
+        ]);
+
+        $this->info("Message added with ID: $response");
 
         // XREAD - чтение записей
         $this->info("Reading messages from stream...");
+
         $response = Redis::command('XREAD', [
-            'BLOCK', 0, // Без блокировки
-            'COUNT', 5, // Чтение до 5 записей
-            'STREAMS', $streamName, '0'
+            'COUNT', 5,         // Чтение до 5 записей
+            'STREAMS',          // Потоки
+            $streamName, '0',   // Название потока и стартовый ID
         ]);
 
-        // Выводим результат
         $this->info("Stream content:");
-        dd($response); // Выведет содержимое потока
+        dd($response); // Вывод содержимого потока
     }
 }
