3e983b4d6843133b7c77450a7fd71cb614299a5e
damert123 <damert337@gmail.com>
2025-01-22 12:30:03 +0300

    queue fix 27

---
+++ app/Console/Commands/TestJson.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Message;
+use Illuminate\Console\Command;
+
+class TestJson extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:test-json';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $message = Message::find(); // Замените 1 на ID нужного сообщения
+        if ($message) {
+            $attachments = json_decode($message->attachments, true);
+            $this->info('Decoded JSON:');
+            print_r($attachments);
+        } else {
+            $this->error('Message not found.');
+        }
+    }
+}
