89fdd8486f2bc8bc4f6dad0e84d5439fe72c54c4
damert123 <damert337@gmail.com>
2025-10-27 15:16:15 +0300

    Добавил TG id

--- app/Services/BasicEventHandler.php
+++ app/Services/BasicEventHandler.php
@@ -191,11 +191,11 @@ class BasicEventHandler extends SimpleEventHandler
                 : "https://t.me/$clientId";
 
             $telegramDataProfileLink = $senderUserName
-                ? "https://t.me/$senderUserName"
+                ? "https://t.me/$senderUserName" . "Telegram ID: {$fromId}"
                 : "ChatId:{$fromId}";
 
             $clientDataTelegramProfileLink = $clientUserName
-                ? "https://t.me/$clientUserName"
+                ? "https://t.me/$clientUserName" ."Telegram ID: {$clientId}"
                 : "ChatId:{$clientId}";
 
             Log::channel('tg-messages')->info($telegramProfileLink);
