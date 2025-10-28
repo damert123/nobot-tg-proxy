58c398288ee77989829ef653f58030acea08c0f8
damert123 <damert337@gmail.com>
2025-10-28 15:01:59 +0300

    изменил сообщееие

--- app/Services/BasicEventHandler.php
+++ app/Services/BasicEventHandler.php
@@ -183,20 +183,20 @@ class BasicEventHandler extends SimpleEventHandler
             }
 
             $telegramProfileLink = $senderUserName
-                ? "https://t.me/$senderUserName  Chat ID: {$fromId}"
-                : "https://t.me/$fromId Chat ID: {$fromId}" ;
+                ? "https://t.me/$senderUserName"
+                : "https://t.me/$fromId";
 
             $clientTelegramProfileLink = $clientUserName
-                ? "https://t.me/$clientUserName Chat ID: {$clientId}"
-                : "https://t.me/$clientId Chat ID: {$clientId}" ;
+                ? "https://t.me/$clientUserName"
+                : "https://t.me/$clientId";
 
             $telegramDataProfileLink = $senderUserName
                 ? "https://t.me/$senderUserName" . " Chat ID: {$fromId}"
-                : "ChatId: {$fromId}";
+                : "Chat ID: {$fromId}";
 
             $clientDataTelegramProfileLink = $clientUserName
                 ? "https://t.me/$clientUserName" ." Chat ID: {$clientId}"
-                : "ChatId: {$clientId}";
+                : "Chat ID: {$clientId}";
 
             Log::channel('tg-messages')->info($telegramProfileLink);
 
